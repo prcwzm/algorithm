864976b33691cd36fee224bf60ae3cfb393519e4
prcwzm <52474337+prcwzm@users.noreply.github.com>
2026-07-22 23:51:48 +0800

    solve group anagrams

--- src/main/java/com/leetcode/hot100/day01/P0049GroupAnagrams.java
+++ src/main/java/com/leetcode/hot100/day01/P0049GroupAnagrams.java
@@ -1,6 +1,9 @@
 package com.leetcode.hot100.day01;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * LeetCode 49. 字母异位词分组
@@ -13,7 +16,41 @@ import java.util.List;
 public class P0049GroupAnagrams {
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        // TODO: 完成字母异位词分组
-        return List.of();
+        HashMap<String, List<String>> map = new HashMap<>();
+
+            for (String str : strs) {
+                String key = constructKey(str);
+//                if (map.containsKey(key)) {
+//                    map.get(key).add(str);
+//                } else {
+//                    List<String> newList = new ArrayList<>();
+//                    newList.add(str);
+//                    map.put(key, newList);
+//                }
+                List<String> list = map.getOrDefault(key, new ArrayList<String>());
+                list.add(str);
+                map.put(key, list);
+            }
+//        List<List<String>> returnLists = new ArrayList<>();
+//        for (Map.Entry<String,List<String>> entry :map.entrySet())
+//        {
+//            returnLists.add(entry.getValue());
+//        }
+//        return returnLists;
+        return new ArrayList<>(map.values());
+    }
+
+    public String constructKey(String str) {
+        int[] mapList = new int[26];
+        for (char c : str.toCharArray()) {
+            int i = c - 'a';
+            mapList[i]++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0 ; i < mapList.length; i++) {
+            sb.append(i).append(":").append(mapList[i]);
+        }
+
+        return sb.toString();
     }
 }
