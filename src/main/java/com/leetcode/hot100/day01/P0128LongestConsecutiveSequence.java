cadc742f17034dec5b842cf7a199b9f4573ef16f
prcwzm <prcwzm@gmail.com>
2026-07-24 21:40:53 +0800

    最大子串提交

--- src/main/java/com/leetcode/hot100/day01/P0128LongestConsecutiveSequence.java
+++ src/main/java/com/leetcode/hot100/day01/P0128LongestConsecutiveSequence.java
@@ -1,17 +1,30 @@
 package com.leetcode.hot100.day01;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * LeetCode 128. 最长连续序列
  * https://leetcode.cn/problems/longest-consecutive-sequence/
- *
- * <p>思路：TODO
- *
- * <p>时间复杂度：TODO；空间复杂度：TODO。
  */
 public class P0128LongestConsecutiveSequence {
 
     public int longestConsecutive(int[] nums) {
-        // TODO: 求最长连续序列的长度
-        return 0;
+        int maxSeq = 0;
+        Set<Integer> numberSet = new HashSet<>();
+        //设置list
+        for (int num : nums){numberSet.add(num);}
+
+        for (int loopNumber : numberSet) {
+            if (numberSet.contains(loopNumber-1)) {
+                continue;
+            }
+            //头节点
+            int currentSeq = 0;
+            while(numberSet.contains(loopNumber + currentSeq)) currentSeq++;
+            maxSeq = Math.max(currentSeq, maxSeq);
+        }
+        return maxSeq;
     }
 }
