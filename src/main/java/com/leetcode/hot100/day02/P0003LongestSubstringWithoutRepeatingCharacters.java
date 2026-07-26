4b565ad061ec244ea3113582559bc520d7c9840a
prcwzm <52474337+prcwzm@users.noreply.github.com>
2026-07-26 15:33:31 +0800

    solve longest substring exercise

--- src/main/java/com/leetcode/hot100/day02/P0003LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/leetcode/hot100/day02/P0003LongestSubstringWithoutRepeatingCharacters.java
@@ -1,11 +1,62 @@
 package com.leetcode.hot100.day02;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /** LeetCode 3. 无重复字符的最长子串。 */
 public class P0003LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String s) {
-        // TODO: 实现后补充思路、时间复杂度和空间复杂度。
-        throw new UnsupportedOperationException("请实现无重复字符的最长子串");
+        // TODO: 在这里练习最优的滑动窗口方案。
+        return lengthOfLongestSubstringFirstAttempt(s);
+    }
+
+    public int lengthOfLongestSubstringFirstAttempt(String s) {
+        if (s.isEmpty()) {
+            return 0;
+        }
+        char[] chars = s.toCharArray();
+        int[] dp = new int[s.length()];
+        Map<Character, Integer> uniMap = new HashMap<>();
+        int maxSeq = 1;
+        int i = 0;
+        dp[0] = 1;
+        for (char c : chars) {
+            if (i == 0) {
+                uniMap.put(c, i);
+                i++;
+                continue;
+            }
+            //判断当前c在Map 中是否存在
+            if (!uniMap.containsKey(c)) {
+                //不存在
+                dp[i] = dp[i-1] + 1;
+            } else {
+                //存在
+                clearItemBefore(uniMap, c);
+                dp[i] = uniMap.size() + 1;
+
+            }
+            maxSeq = Math.max(maxSeq,dp[i]);
+            uniMap.put(c,i);
+            i++;
+        }
+        return  maxSeq;
+    }
+
+    public void clearItemBefore(Map<Character, Integer> uniMap, char s) {
+        int i = uniMap.get(s);
+        List<Character> characters = new ArrayList<>();
+        for (Map.Entry<Character, Integer> entry : uniMap.entrySet()) {
+            if (entry.getValue() <= i) {
+                characters.add(entry.getKey());
+            }
+        }
+        for (Character c : characters) {
+            uniMap.remove(c);
+        }
+
     }
 }
-
--- src/test/java/com/leetcode/hot100/day02/P0003LongestSubstringWithoutRepeatingCharactersTest.java
+++ src/test/java/com/leetcode/hot100/day02/P0003LongestSubstringWithoutRepeatingCharactersTest.java
@@ -2,10 +2,8 @@ package com.leetcode.hot100.day02;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled("实现题解后删除此注解")
 class P0003LongestSubstringWithoutRepeatingCharactersTest {
     private final P0003LongestSubstringWithoutRepeatingCharacters solution =
             new P0003LongestSubstringWithoutRepeatingCharacters();
@@ -24,5 +22,44 @@ class P0003LongestSubstringWithoutRepeatingCharactersTest {
     void handlesEmptyString() {
         assertEquals(0, solution.lengthOfLongestSubstring(""));
     }
-}
 
+    @Test
+    void movesPastDuplicateInsideCurrentWindow() {
+        assertEquals(2, solution.lengthOfLongestSubstring("abba"));
+    }
+
+    @Test
+    void keepsCharacterWhoseOldOccurrenceLeftTheWindow() {
+        assertEquals(3, solution.lengthOfLongestSubstring("dvdf"));
+    }
+
+    @Test
+    void preservesHistoricalMaximumAfterWindowShrinks() {
+        assertEquals(3, solution.lengthOfLongestSubstring("pwwkew"));
+    }
+
+    @Test
+    void handlesSeveralWindowMoves() {
+        assertEquals(5, solution.lengthOfLongestSubstring("tmmzuxt"));
+    }
+
+    @Test
+    void treatsSpaceAsACharacter() {
+        assertEquals(1, solution.lengthOfLongestSubstring(" "));
+    }
+
+    @Test
+    void handlesTwoDifferentCharacters() {
+        assertEquals(2, solution.lengthOfLongestSubstring("au"));
+    }
+
+    @Test
+    void handlesRepeatedCharacterAfterPreviousWindowShrink() {
+        assertEquals(3, solution.lengthOfLongestSubstring("cbcba"));
+    }
+
+    @Test
+    void findsLongWindowAfterMultipleEarlierDuplicates() {
+        assertEquals(7, solution.lengthOfLongestSubstring("bpfbhmipx"));
+    }
+}
