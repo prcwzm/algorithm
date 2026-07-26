ca151bba8239a0c03bd89da0157ee622cfc5ea3f
prcwzm <52474337+prcwzm@users.noreply.github.com>
2026-07-26 18:32:18 +0800

    solve find all anagrams

--- src/main/java/com/leetcode/hot100/day02/P0438FindAllAnagramsInAString.java
+++ src/main/java/com/leetcode/hot100/day02/P0438FindAllAnagramsInAString.java
@@ -1,13 +1,62 @@
 package com.leetcode.hot100.day02;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /** LeetCode 438. 找到字符串中所有字母异位词。 */
 public class P0438FindAllAnagramsInAString {
 
+    public List<Integer> findAnagrams1(String s, String p) {
+        if (s.length() < p.length()) {
+            return new ArrayList<>();
+        }
+        List<Integer> res = new ArrayList<>();
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
+
+        for(int i = 0; i < p.length(); i++) {
+            pCount[p.charAt(i) - 'a']++;
+            sCount[s.charAt(i) - 'a']++;
+        }
+        if (Arrays.equals(sCount, pCount)) {
+            res.add(0);
+        }
+
+        for (int i = 0; i < s.length() - p.length(); i++) {
+            sCount[s.charAt(i) - 'a']--;
+            sCount[s.charAt(i + p.length()) - 'a']++;
+            if (Arrays.equals(sCount, pCount)) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+
     public List<Integer> findAnagrams(String s, String p) {
-        // TODO: 实现后补充思路、时间复杂度和空间复杂度。
-        throw new UnsupportedOperationException("请实现找到字符串中所有字母异位词");
+        if (s.length() < p.length()) {
+            return new ArrayList<>();
+        }
+        List<Integer> res = new ArrayList<>();
+        int[] pCount = new int[26];
+
+        for(int i = 0; i < p.length(); i++) {
+            pCount[p.charAt(i) - 'a']++;
+        }
+        int left = 0;
+        for (int right = 0; right < s.length(); right ++) {
+            int  c = s.charAt(right) - 'a';
+            pCount[c]--;
+            //右边入队
+            while (pCount[c]<0) {
+                pCount[s.charAt(left) - 'a']++;
+                left++;
+            }
+            if (right - left + 1 == p.length()) {
+                res.add(left);
+            }
+        }
+        return res;
     }
 }
 
--- src/test/java/com/leetcode/hot100/day02/P0438FindAllAnagramsInAStringTest.java
+++ src/test/java/com/leetcode/hot100/day02/P0438FindAllAnagramsInAStringTest.java
@@ -3,10 +3,8 @@ package com.leetcode.hot100.day02;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled("实现题解后删除此注解")
 class P0438FindAllAnagramsInAStringTest {
     private final P0438FindAllAnagramsInAString solution = new P0438FindAllAnagramsInAString();
 
@@ -19,5 +17,19 @@ class P0438FindAllAnagramsInAStringTest {
     void findsOverlappingAnagrams() {
         assertEquals(List.of(0, 1, 2), solution.findAnagrams("abab", "ab"));
     }
-}
 
+    @Test
+    void returnsEmptyWhenPatternIsLongerThanSource() {
+        assertEquals(List.of(), solution.findAnagrams("ab", "abc"));
+    }
+
+    @Test
+    void respectsRepeatedCharacterCounts() {
+        assertEquals(List.of(1), solution.findAnagrams("baa", "aa"));
+    }
+
+    @Test
+    void returnsEmptyWhenNoAnagramExists() {
+        assertEquals(List.of(), solution.findAnagrams("abcd", "ee"));
+    }
+}
