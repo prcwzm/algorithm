b6a954101817eceb9aa7c081e4d998d970f8621d
prcwzm <52474337+prcwzm@users.noreply.github.com>
2026-08-02 00:13:01 +0800

    solve rotate image

--- src/main/java/com/leetcode/hot100/day04/P0048RotateImage.java
+++ src/main/java/com/leetcode/hot100/day04/P0048RotateImage.java
@@ -4,7 +4,22 @@ package com.leetcode.hot100.day04;
 public class P0048RotateImage {
 
     public void rotate(int[][] matrix) {
-        // TODO: 实现后补充思路、时间复杂度和空间复杂度。
-        throw new UnsupportedOperationException("请实现旋转图像");
+        int ml = matrix.length;
+        for (int i = 0; i < matrix.length / 2; i++) {
+            for (int j = 0; j < (matrix.length + 1) / 2 ; j ++) {
+                fourPointChange(matrix, i, j, ml);
+            }
+        }
+    }
+
+    public void fourPointChange(int[][] matrix, int i, int j, int ml) {
+        int m1 = matrix[i][j];
+        int m2 = matrix[j][ml - 1 - i];
+        int m3 = matrix[ml - 1 - i][ml - 1 - j];
+        int tmp = matrix[ml - 1 - j][i];
+        matrix[ml - 1 - j][i] = m3;
+        matrix[ml - 1 - i][ml - 1 - j] = m2;
+        matrix[j][ml - 1 - i] = m1;
+        matrix[i][j] = tmp;
     }
 }
--- src/test/java/com/leetcode/hot100/day04/P0048RotateImageTest.java
+++ src/test/java/com/leetcode/hot100/day04/P0048RotateImageTest.java
@@ -2,10 +2,8 @@ package com.leetcode.hot100.day04;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled("实现题解后删除此注解")
 class P0048RotateImageTest {
     private final P0048RotateImage solution = new P0048RotateImage();
 
@@ -21,5 +19,24 @@ class P0048RotateImageTest {
         int[][] matrix = {{1, 2}, {3, 4}};
         solution.rotate(matrix);
         assertArrayEquals(new int[][] {{3, 1}, {4, 2}}, matrix);
+    }
+
+    @Test
+    void rotatesFourByFourMatrixClockwise() {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+        solution.rotate(matrix);
+        assertArrayEquals(
+                new int[][] {
+                    {13, 9, 5, 1},
+                    {14, 10, 6, 2},
+                    {15, 11, 7, 3},
+                    {16, 12, 8, 4}
+                },
+                matrix);
     }
 }
