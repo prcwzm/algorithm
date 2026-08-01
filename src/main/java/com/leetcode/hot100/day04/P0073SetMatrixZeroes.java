5b2e84f75015997015cf43fe39641da91e28ee57
prcwzm <52474337+prcwzm@users.noreply.github.com>
2026-08-01 22:36:03 +0800

    solve set matrix zeroes

--- src/main/java/com/leetcode/hot100/day04/P0073SetMatrixZeroes.java
+++ src/main/java/com/leetcode/hot100/day04/P0073SetMatrixZeroes.java
@@ -4,7 +4,48 @@ package com.leetcode.hot100.day04;
 public class P0073SetMatrixZeroes {
 
     public void setZeroes(int[][] matrix) {
-        // TODO: 实现后补充思路、时间复杂度和空间复杂度。
-        throw new UnsupportedOperationException("请实现矩阵置零");
+        int colSize = matrix[0].length;
+        int rowSize = matrix.length;
+        boolean rowZero = false;
+        boolean colZero = false;
+        for (int col = 0; col < colSize; col++) {
+            for (int row = 0; row < rowSize; row++) {
+                if (matrix[row][col] == 0) {
+                    matrix[row][0] = 0;
+                    matrix[0][col] = 0;
+                    if (row == 0){
+                        rowZero = true;
+                    }
+                    if (col==0){
+                        colZero = true;
+                    }
+                }
+            }
+        }
+
+        for (int col = 1; col < colSize; col++) {
+            if (matrix[0][col] == 0) {
+                for (int row = 1; row < rowSize; row++) {
+                        matrix[row][col] = 0;
+                }
+            }
+        }
+        for (int row = 1 ; row < rowSize; row++) {
+            if (matrix[row][0] == 0) {
+                for (int col = 1 ; col < colSize; col ++) {
+                    matrix[row][col] = 0;
+                }
+            }
+        }
+        if (rowZero) {
+            for (int i = 0; i < colSize; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (colZero) {
+            for (int i = 0; i < rowSize; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
--- src/test/java/com/leetcode/hot100/day04/P0073SetMatrixZeroesTest.java
+++ src/test/java/com/leetcode/hot100/day04/P0073SetMatrixZeroesTest.java
@@ -2,10 +2,8 @@ package com.leetcode.hot100.day04;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled("实现题解后删除此注解")
 class P0073SetMatrixZeroesTest {
     private final P0073SetMatrixZeroes solution = new P0073SetMatrixZeroes();
 
@@ -21,5 +19,19 @@ class P0073SetMatrixZeroesTest {
         int[][] matrix = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
         solution.setZeroes(matrix);
         assertArrayEquals(new int[][] {{0, 0, 0, 0}, {0, 4, 5, 0}, {0, 3, 1, 0}}, matrix);
+    }
+
+    @Test
+    void handlesZeroInLastRow() {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 0}};
+        solution.setZeroes(matrix);
+        assertArrayEquals(new int[][] {{1, 2, 0}, {0, 0, 0}}, matrix);
+    }
+
+    @Test
+    void zeroesSingleRowContainingZero() {
+        int[][] matrix = {{1, 0, 3}};
+        solution.setZeroes(matrix);
+        assertArrayEquals(new int[][] {{0, 0, 0}}, matrix);
     }
 }
