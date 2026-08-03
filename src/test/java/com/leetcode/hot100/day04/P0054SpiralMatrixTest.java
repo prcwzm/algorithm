49756402b97027a8e8fe41459f7c6a98d6291081
prcwzm <52474337+prcwzm@users.noreply.github.com>
2026-08-03 23:08:17 +0800

    solve spiral matrix

--- src/test/java/com/leetcode/hot100/day04/P0054SpiralMatrixTest.java
+++ src/test/java/com/leetcode/hot100/day04/P0054SpiralMatrixTest.java
@@ -3,10 +3,8 @@ package com.leetcode.hot100.day04;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled("实现题解后删除此注解")
 class P0054SpiralMatrixTest {
     private final P0054SpiralMatrix solution = new P0054SpiralMatrix();
 
@@ -23,5 +21,15 @@ class P0054SpiralMatrixTest {
                 List.of(1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7),
                 solution.spiralOrder(
                         new int[][] {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}));
+    }
+
+    @Test
+    void traversesSingleRowWithoutDuplicates() {
+        assertEquals(List.of(1, 2, 3), solution.spiralOrder(new int[][] {{1, 2, 3}}));
+    }
+
+    @Test
+    void traversesSingleColumnWithoutDuplicates() {
+        assertEquals(List.of(1, 2, 3), solution.spiralOrder(new int[][] {{1}, {2}, {3}}));
     }
 }
